30e930b3ddb8e87625ee32002598b307b3cf2ca0
YOUR NAME <ritter.christina.r.c@gmail.com>
2023-02-21 14:36:26 +0100

    dynamic routing implemented

--- art-gallery-app/component/ArtPieceDetails.js
+++ art-gallery-app/component/ArtPieceDetails.js
@@ -1,13 +1,17 @@
+import Image from "next/image"
+import Link from "next/link"
 
-export default function ArtPieceDetails({image, title, artist, year, genre}) {
+export default function ArtPieceDetails({pieces, id}) {
+const artPieceObject = pieces.find((piece) => piece.slug === id )
 
     return (
         <>
-            <Image className="details__image"src={image}></Image>
-            <h2 className="details__title">{title}</h2>
-            <h3 className="details__artist">{artist}</h3>
-            <div className="details__year">{year}</div>
-            <div className="details_genre">{genre}</div>
+            <Image className="details__image" src={artPieceObject.imageSource} width={400} height={500}></Image>
+            <h2 className="details__title">{artPieceObject.title}</h2>
+            <h3 className="details__artist">{artPieceObject.artist}</h3>
+            <div className="details__year">{artPieceObject.year}</div>
+            <div className="details_genre">{artPieceObject.genre}</div>
+            <Link href="/art-pieces">Back</Link>
         </>
     )
 }
