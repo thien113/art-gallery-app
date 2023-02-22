97eb58348ddf05cad94067381d8e13860537931d
YOUR NAME <ritter.christina.r.c@gmail.com>
2023-02-22 13:19:16 +0100

    save filter which is working but not rendering favImg

--- art-gallery-app/pages/favorites.js
+++ art-gallery-app/pages/favorites.js
@@ -1,11 +1,15 @@
-export default function FavoriteFilter({ artPiecesInfo, onToggleFavorite }) {
+export default function FavoriteFilter({ artPiecesInfo}) {
   return (
     <>
       <h2>My Favorites</h2>
       {artPiecesInfo
         .filter((artPieceInfo) => artPieceInfo.isFavorite === true)
         .map((favoritePieces) => (
+            <> 
+            <h2>{favoritePieces.title}</h2>
+            <h3>{favoritePieces.artist}</h3>
           <div>{favoritePieces.slug}</div>
+            </>
         ))}
     </>
   );
