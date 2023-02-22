c36c83e1210b508b23296a074fe6a1cba36d0488
CeMaGo <cecilemarleen@gmail.com>
2023-02-22 21:24:38 +0100

    writing async text for input field, having some trouble with the targeting, to be continued

--- art-gallery-app/component/comment/CommentForm.test.js
+++ art-gallery-app/component/comment/CommentForm.test.js
@@ -1,7 +1,9 @@
 import CommentForm from "./CommentForm";
 
-import { getByRole, render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
 
+// <--- getting by queryByTitle for changing State, might be overkill... will try with getByTitle as well--->
 it("inputRenderCheck", () => {
   const { queryByTitle } = render(<CommentForm />);
   const input = queryByTitle("dummyInputTitle");
@@ -17,8 +19,19 @@ describe("is the Input from the CommentForm being rendered to the screen", () =>
 
   test("Is the SubmitButton being rendered?", () => {
     const { getByDisplayValue } = render(<CommentForm />);
-    const btnSubmit = getByDisplayValue("submit");
+    const btnSubmit = getByDisplayValue("Create a Comment");
     expect(btnSubmit).toBeTruthy();
+  });
+
+  // <--- usinhg act()wrapper for State change to be rendered on screen, async-await for test input to be checked--!-->
+  test("change on Input is appearing in the Comments-Inputfield", async () => {
+    await act(async () => {
+      const { getByTitle } = render(<CommentForm />);
+      const inputfield = getByTitle("dummyInputTitle");
+      const DummyText = "Is this Art? oder kann das weg...";
+      await fireEvent.change(inputfield, { target: { value: DummyText } });
+      expect(inputfield).toBe(DummyText);
+    });
   });
 });
 
