8a4737c79a04e3935658cbdcea70ff08024d5b23
YOUR NAME <ritter.christina.r.c@gmail.com>
2023-02-22 11:55:50 +0100

    favorites.js edited

--- art-gallery-app/pages/favorites.js
+++ art-gallery-app/pages/favorites.js
@@ -1,9 +1,12 @@
-export default function FavoriteFilter ({artPiecesInfo, onToggleFavorite}){
-    return (
-        <h2>My Favorites</h2>
-        // {artPiecesInfo.filter((artPiecesInfo) => artPiecesInfo.slug.isFavorite===true)(
-        //     return 
-        // )}
-    )
+export default function FavoriteFilter({ artPiecesInfo, onToggleFavorite }) {
+  return (
+    <>
+      <h2>My Favorites</h2>
+      {artPiecesInfo
+        .filter((artPieceInfo) => artPieceInfo.isFavorite === true)
+        .map((favoritePieces) => (
+          <div>{favoritePieces.slug}</div>
+        ))}
+    </>
+  );
 }
-
