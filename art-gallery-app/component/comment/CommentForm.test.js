9b1c21b4e0e6667f6b530e3f75d968e453679951
CeMaGo <cecilemarleen@gmail.com>
2023-02-22 18:45:59 +0100

    still trying to get changes to be committed

--- art-gallery-app/component/comment/CommentForm.test.js
+++ art-gallery-app/component/comment/CommentForm.test.js
@@ -1,10 +1,17 @@
+import { iteratorSymbol } from "immer/dist/internal";
+import CommentForm from "./CommentForm";
 
+it("inputRenderCheck", () => {
+  const { queryByTitle } = render(<CommentForm />);
+  const input = queryByTitle("dummyInputTitle");
+  expect(input).toBeTruthy();
+});
 
-const dummyComment = "Is this art?"
+// const dummyComment = "Is this art?"
 
-test('should receive a text Input', () => {
- 
- const input =
- 
-    export(input).toBeInTheDocument()
-})
+// test('should receive a text Input', () => {
+
+//  const input =
+
+//     export(input).toBeInTheDocument()
+// })
